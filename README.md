f142aa5fdcd7e8ec72c63827f799cd1b9417369e
0xLeia <0xleia@protonmail.com>
2021-01-10 18:50:12 -0500

    Add UnicSwap code

---
+++ README.md
@@ -0,0 +1,24 @@
+# UnicSwap
+
+UnicSwap is a fork of [Uniswap V2](https://github.com/Uniswap/uniswap-v2-core) by `0xLeia` for [unicly](https://unic.ly)
+
+The changes made are:
+- Switch contract versions to 0.6.12 and patch code accordingly
+- Change contract and variable names for UnicSwap
+- New init code hash
+
+# Local Development
+
+The following assumes the use of `node@>=10`.
+
+## Install Dependencies
+
+`yarn`
+
+## Compile Contracts
+
+`yarn compile`
+
+## Run Tests
+
+`yarn test`
---
+++ test/shared/fixtures.ts
@@ -0,0 +1,105 @@
+import { Contract, Wallet } from 'ethers'
+import { Web3Provider } from 'ethers/providers'
+import { deployContract } from 'ethereum-waffle'
+
+import { expandTo18Decimals } from './utilities'
+
+import ERC20 from '../../build/ERC20.json'
+import UnicSwapV2Factory from '../../build/UnicSwapV2Factory.json'
+import UnicSwapV2Pair from '../../build/UnicSwapV2Pair.json'
+import WETH9 from '../../build/WETH9.json'
+import IUnicSwapV2Pair from '../../build/IUnicSwapV2Pair.json'
+import UnicSwapV2Router02 from '../../build/UnicSwapV2Router02.json'
+import RouterEventEmitter from '../../build/RouterEventEmitter.json'
+
+interface FactoryFixture {
+  factory: Contract
+}
+
+const overrides = {
+  gasLimit: 9999999
+}
+
+export async function factoryFixture(_: Web3Provider, [wallet]: Wallet[]): Promise<FactoryFixture> {
+  const factory = await deployContract(wallet, UnicSwapV2Factory, [wallet.address], overrides)
+  return { factory }
+}
+
+interface PairFixture extends FactoryFixture {
+  token0: Contract
+  token1: Contract
+  pair: Contract
+}
+
+export async function pairFixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<PairFixture> {
+  const { factory } = await factoryFixture(provider, [wallet])
+
+  const tokenA = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
+  const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
+
+  await factory.createPair(tokenA.address, tokenB.address, overrides)
+  const pairAddress = await factory.getPair(tokenA.address, tokenB.address)
+  const pair = new Contract(pairAddress, JSON.stringify(UnicSwapV2Pair.abi), provider).connect(wallet)
+
+  const token0Address = (await pair.token0()).address
+  const token0 = tokenA.address === token0Address ? tokenA : tokenB
+  const token1 = tokenA.address === token0Address ? tokenB : tokenA
+
+  return { factory, token0, token1, pair }
+}
+
+interface V2Fixture {
+  token0: Contract,
+  token1: Contract,
+  WETH: Contract
+  WETHPartner: Contract
+  factoryV2: Contract
+  router02: Contract
+  routerEventEmitter: Contract
+  router: Contract
+  pair: Contract
+  WETHPair: Contract
+}
+
+export async function v2Fixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<V2Fixture> {  
+  // deploy tokens
+  const tokenA = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+  const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+  const WETH = await deployContract(wallet, WETH9)
+  const WETHPartner = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+
+  // deploy V2
+  const factoryV2 = await deployContract(wallet, UnicSwapV2Factory, [wallet.address])
+
+  // deploy routers
+  const router02 = await deployContract(wallet, UnicSwapV2Router02, [factoryV2.address, WETH.address], overrides)
+
+  // event emitter for testing
+  const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [])
+
+  // initialize V2
+  await factoryV2.createPair(tokenA.address, tokenB.address)
+  const pairAddress = await factoryV2.getPair(tokenA.address, tokenB.address)
+  const pair = new Contract(pairAddress, JSON.stringify(IUnicSwapV2Pair.abi), provider).connect(wallet)
+
+  const token0Address = await pair.token0()
+  const token0 = tokenA.address === token0Address ? tokenA : tokenB
+  const token1 = tokenA.address === token0Address ? tokenB : tokenA
+
+  await factoryV2.createPair(WETH.address, WETHPartner.address)
+  const WETHPairAddress = await factoryV2.getPair(WETH.address, WETHPartner.address)
+  const WETHPair = new Contract(WETHPairAddress, JSON.stringify(IUnicSwapV2Pair.abi), provider).connect(wallet)
+
+  return { 
+    token0,
+    token1,
+    pair,
+    WETH,
+    WETHPartner,
+    factoryV2,
+    router02,
+    router: router02,
+    routerEventEmitter,
+    WETHPair
+  }
+}
